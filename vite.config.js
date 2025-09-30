4c7bfa2ba5959b8103e3dcc1a4c07c65bab16616
Katherin2024 <heidy.avilesp@uniagustiniana.edu.co>
2025-09-30 02:41:20 +0000

    segundocambio

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Deploy/', // 👈 aquí va el nombre exacto de tu repositorio en GitHub
 })
